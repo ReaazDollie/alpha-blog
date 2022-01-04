f2c877ea011f9e96c1af263227a6ea07bde6710c
reaaz <reaaz77@gmail.com>
2022-01-04 08:42:58 +0200

    created signup form for users

--- db/migrate/20220103100848_add_password_to_users.rb
+++ db/migrate/20220103100848_add_password_to_users.rb
@@ -1,9 +1,5 @@
 class AddPasswordToUsers < ActiveRecord::Migration[6.1]
   def change
-<<<<<<< HEAD
     add_column :users, :password_digest, :string 
-=======
-    add_column :users, :password_digest, :string
->>>>>>> users-signup
   end
 end
